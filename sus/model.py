18ee4293f07167d99a8c534596ad579b2debe6a2
Mikolaj Pytlak <mikolaj.piotr.pytlak@gmail.com>
2022-09-26 22:03:15 +0200

    testing cnn models

--- sus/model.py
+++ sus/model.py
@@ -8,47 +8,50 @@ import tensorflow as tf
 
 """ Weird errors """
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 
 """ CONSTANT VARS """
-CODEBLOCK = 0
 DATA_DIR = ".\\data\\images\\.train"
 SAVE_DIR = ".\\sus\\models"
-
 
 def main():
 
     outputs = len(list(Path(DATA_DIR).iterdir()))
 
+    input = 64
+    batch = 16
+
     ds_train, ds_test = tf.keras.utils.image_dataset_from_directory(
         directory=DATA_DIR,
-        label_mode='int',
-        batch_size=8,
-        image_size=(28, 28),
+        label_mode="int",
+        batch_size=batch,
+        image_size=(input, input),
         validation_split=0.2,
         seed=123,
         subset="both",
-        color_mode='grayscale',
+        color_mode="rgb",
     )
 
     model = tf.keras.Sequential(
         [
-            # Input layers
-            tf.keras.layers.Input((28, 28, 1)),
-            tf.keras.layers.Conv2D(32, (3, 3), padding="valid", activation='relu'),
-            
-            # Maxpool Conv2D
+            tf.keras.layers.Input((input, input, 3)),
+            # 
+            tf.keras.layers.Conv2D(32, 3, 1, padding='same', activation='relu'),
             tf.keras.layers.MaxPooling2D(),
-            tf.keras.layers.Conv2D(64, (3, 3), padding="valid", activation='relu'),
-
-            # Maxpool Conv2D
+            # 
+            tf.keras.layers.Conv2D(64, 3, 1, padding='same', activation='relu'),
             tf.keras.layers.MaxPooling2D(),
-            tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
-
-            # Output layers
+            # 
+            tf.keras.layers.Conv2D(128, 3, 1, padding='same', activation='relu'),
+            tf.keras.layers.MaxPooling2D(),
+            # 
+            tf.keras.layers.Conv2D(256, 3, 1, padding='same', activation='relu'),
+            tf.keras.layers.MaxPooling2D(),
+            # 
             tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(64, activation='relu'),            
+            tf.keras.layers.Dense(128, activation='relu'),
             tf.keras.layers.Dense(outputs),
         ]
     )
@@ -59,12 +62,15 @@ def main():
         metrics=["accuracy"],
     )
 
-    if False:
-        model.fit(ds_train, batch_size=16, epochs=15, verbose=2)
-        model.save(f"{SAVE_DIR}\\model_02")
-    else:
-        model = tf.keras.models.load_model(f"{SAVE_DIR}\\model_02")
-        model.evaluate(ds_test, batch_size=16, verbose=2)
+    model.summary()
+
+    # model.fit(ds_train, batch_size=batch, epochs=30, verbose=2)
+    # model.save(f"{SAVE_DIR}\\model09")
+
+    model = tf.keras.models.load_model(f"{SAVE_DIR}\\model09")
+
+    # model.evaluate(ds_test, batch_size=batch, verbose=2)
+
 
 
 
