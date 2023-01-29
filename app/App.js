cb36576db445e7471cecdd319591718fcd3a78f0
Patryk Koc <kefo321@gmail.com>
2023-01-29 21:07:41 +0100

    Dodałem wiadomość o nazwie "CHUJ_CHUJ"

--- app/App.js
+++ app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, Image } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* Tutaj leci kod aplikacji. Nie dodawajcie jeszcze NativeWinda */}
+      {/* Tutaj leci kod aplikacji. Nie dodawajcie jeszcze Native CHUJ_CHUJ Winda */}
       <StatusBar style="auto" />
     </View>
   );
