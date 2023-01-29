4e2c4c024302d1f476f954c94dcfb7ed788c0f69
Mikolaj Pytlak <mikolaj.piotr.pytlak@gmail.com>
2023-01-29 20:14:13 +0100

    added one comment

--- app/App.js
+++ app/App.js
@@ -1,19 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>MOJA POGGERS APPKA</Text>
-      <Text> </Text>
-      <Text> </Text>
-      <Text>KIEDY MENTORKA POWIE, ŻEBY</Text>
-      <Image 
-        source={
-          require("./images/image.png")
-        }/>
-        <Text>ŻEBY WBIĆ 3,000 ZŁ I 30,000</Text>
-        <Text>FOLLOWÓW NA FB</Text>
+      {/* Tutaj leci kod aplikacji. Nie dodawajcie jeszcze NativeWinda */}
       <StatusBar style="auto" />
     </View>
   );
@@ -22,13 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  title: {
-    color: 'red',
-    fontSize: 20,
-    fontWeight: 'bold',
-  }
 });
