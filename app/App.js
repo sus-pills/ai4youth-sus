03eb9909237a7bdbe67bb5add04f38c127609f8e
Patryk Koc <kefo321@gmail.com>
2023-02-01 20:07:24 +0100

    Zrobiłem Drawera, Zakładki i ogólnie projekt aplikacji

--- app/App.js
+++ app/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import {Calendar} from 'react-native-calendars';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Home from './containers/home';
+import Wpisy from './containers/wpisy';
+import Kalendarz from './containers/kalendarz';
+import Ustawienia from './containers/ustawienia';
+import Detektor from './containers/detektor';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Drawer = createDrawerNavigator();
+
+const Styles = StyleSheet.create({
+  zakladka: {
+    backgroundColor: 'black',
+    width: 200,
+  }
+  },
+);
 
 export default function App() {
   return (
     /* 
-    
-    Kalendarz
+
+    Kalendarz 1
 
     <View>
       <CalendarList
@@ -17,63 +34,61 @@ export default function App() {
   showScrollIndicator={true}
       />  
     </View>
-    
-    */
+
+    Kalendarz 2
 
     <View>
       <Calendar
-  // Initially visible month. Default = now
   initialDate={'2023-01-31'}
-  // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
   minDate={'2023-01-31'}
-  // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
   maxDate={'2023-02-25'}
-  // Handler which gets executed on day press. Default = undefined
   onDayPress={day => {
     console.log('selected day', day);
   }}
-  // Handler which gets executed on day long press. Default = undefined
   onDayLongPress={day => {
     console.log('selected day', day);
   }}
-  // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
   monthFormat={'yyyy MM'}
-  // Handler which gets executed when visible month changes in calendar. Default = undefined
   onMonthChange={month => {
     console.log('month changed', month);
   }}
-  // Hide month navigation arrows. Default = false
   hideArrows={true}
-  // Replace default arrows with custom ones (direction can be 'left' or 'right')
   renderArrow={direction => <Arrow />}
-  // Do not show days of other months in month page. Default = false
   hideExtraDays={true}
-  // If hideArrows = false and hideExtraDays = false do not switch month when tapping on greyed out
-  // day from another month that is visible in calendar page. Default = false
   disableMonthChange={true}
-  // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
   firstDay={1}
-  // Hide day names. Default = false
   hideDayNames={true}
-  // Show week numbers to the left. Default = false
   showWeekNumbers={true}
-  // Handler which gets executed when press arrow icon left. It receive a callback can go back month
   onPressArrowLeft={subtractMonth => subtractMonth()}
-  // Handler which gets executed when press arrow icon right. It receive a callback can go next month
   onPressArrowRight={addMonth => addMonth()}
-  // Disable left arrow. Default = false
   disableArrowLeft={true}
-  // Disable right arrow. Default = false
   disableArrowRight={true}
-  // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
   disableAllTouchEventsForDisabledDays={true}
-  // Replace default month and year title with custom one. the function receive a date as parameter
   renderHeader={date => {
-    /*Return JSX*/
   }}
-  // Enable the option to swipe between months. Default = false
   enableSwipeMonths={true}
 /> 
     </View>
+    */
+   <NavigationContainer>
+   <Drawer.Navigator screenOptions={{
+    headerShown: true,
+    headerStyle: {
+      backgroundColor: '#1A5A7D',
+    }
+  }}
+   drawerStyle={{
+    width: 280,
+    backgroundColor: '#1A5A7D',
+   }}
+   initialRouteName="Home">
+    <Drawer.Screen name="Start" component={Home} />
+    <Drawer.Screen name="Wpisy" component={Wpisy} /> 
+    <Drawer.Screen name="Kalendarz" component={Kalendarz} /> 
+    <Drawer.Screen name="Detektor Leków" component={Detektor} />
+    <Drawer.Screen name="Ustawienia" component={Ustawienia} /> 
+
+   </Drawer.Navigator>
+   </NavigationContainer>
   );
 };
--- app/containers/ustawienia.js
+++ app/containers/ustawienia.js
@@ -1,8 +1,53 @@
 import React from "react";
-import { Text } from "react-native";
-function Ustawienia() {
+import { View,Text, StyleSheet, Button, Switch, TextInput } from "react-native";
+import OptionsMenu from "react-native-option-menu";
+const MyComponent = () => {
+    const [isSwitchOn, setIsSwitchOn] = React.useState(false);
+    const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
+    return <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />;
+  };
+
+const okoIkona = require("../img/eye.png");
+const textIkona = require("../img/text.png");
+
+const Styles = StyleSheet.create({
+    container: {
+        padding: 25,
+        alignItems: 'center',
+        justifyContent: 'center',
+        verticalAlign: 'middle',
+        backgroundColor: '#0E2A3E',
+        height: 723,
+    },
+    TekstAplikacji: {
+        fontSize: 21,
+        color: 'white',
+        fontFamily: 'sans-serif-condensed', 
+        textAlign: 'left',
+    },
+  });
+
+export default function Ustawienia() {
     return (
-        <Text>xdd</Text>
-    )
-}
-export default Ustawienia
+        <View style={Styles.container}>
+
+            <Text style={Styles.TekstAplikacji}>Tryb dla daltonistów:</Text>
+            <Switch></Switch>
+
+            <Text style={Styles.TekstAplikacji}>Wysoki kontrast:</Text>
+            <Switch></Switch>
+
+            <OptionsMenu
+            button={okoIkona}
+            buttonStyle={{ width: 55, height: 55, margin: 7.5, resizeMode: "contain" }}
+            options={["Trinatopia", "Protanopia"]} 
+            />
+            <OptionsMenu
+            button={textIkona}
+            buttonStyle={{ width: 55, height: 55, margin: 7.5, resizeMode: "contain" }}
+            options={["32", "36","40"]} 
+            />
+        </View>
+    );
+};
+/* W switchu jeśli tryb dla daltonistów pokażą się typy. Trinatopia i tym podobne*/
