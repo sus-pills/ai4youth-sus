1d265e9c0ef1742b57b57db7a1e2c75bfd2d0cd4
Mikolaj Pytlak <mikolaj.piotr.pytlak@gmail.com>
2023-02-03 00:27:21 +0100

    Added icons to drawer

--- app/App.js
+++ app/App.js
@@ -1,6 +1,7 @@
 // Imports
-import React from "react";
+import React, { useState } from "react";
 import { Text, View } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 // Navigation Imports
 import { NavigationContainer } from "@react-navigation/native";
@@ -17,25 +18,61 @@ import { CustomColors } from "./global/globalStyles";
 const Drawer = createDrawerNavigator();
 
 export default function App() {
+  const activeColor = CustomColors.customMain;
+  const inactiveColor = CustomColors.customDarkGray;
+
   return (
     <NavigationContainer>
       <Drawer.Navigator
         screenOptions={{
           headerShown: true,
           headerStyle: { backgroundColor: CustomColors.customMain },
-          headerTitleStyle: { color: "white"},
           headerTitleAlign: "center",
-        }}
-        drawerStyle={{
-          width: 280,
-          backgroundColor: "#1A5A7D",
+          headerTintColor: 'white',
+          drawerActiveTintColor: activeColor,
+          drawerInactiveTintColor: inactiveColor,
         }}
         initialRouteName="PillCalendar"
       >
-        <Drawer.Screen name="Kalendarz" component={PillCalendar} />
-        <Drawer.Screen name="Wykryj lek" component={Detector} />
-        <Drawer.Screen name="Wpisy" component={Entries} />
-        <Drawer.Screen name="Ustawienia" component={Settings} />
+        <Drawer.Screen name="Kalendarz" component={PillCalendar} options={{
+          title: 'Kalendarz',
+          drawerIcon: ({focused, size}) => <MaterialIcons
+            name={'calendar-today'}
+            size={size}
+            color={focused ? CustomColors.customMain : inactiveColor}
+            // style={{ marginRight: 8 }}
+          />
+        }} />
+        
+        <Drawer.Screen name="Wykryj lek" component={Detector} options={{
+          title: 'Wykryj lek',
+          drawerIcon: ({focused, size}) => <MaterialIcons
+            name={'photo-camera'}
+            size={size}
+            color={focused ? CustomColors.customMain : inactiveColor}
+            // style={{ marginRight: 8 }}
+          />
+        }} />
+
+        <Drawer.Screen name="Wpisy" component={Entries} options={{
+          title: 'Wpisy',
+          drawerIcon: ({focused, size}) => <MaterialIcons
+            name={'format-list-bulleted'}
+            size={size}
+            color={focused ? CustomColors.customMain : inactiveColor}
+            // style={{ marginRight: 8 }}
+          />
+        }} />
+
+        <Drawer.Screen name="Ustawienia" component={Settings} options={{
+          title: 'Ustawienia',
+          drawerIcon: ({focused, size}) => <MaterialIcons
+            name={'settings'}
+            size={size}
+            color={focused ? CustomColors.customMain : inactiveColor}
+            // style={{ marginRight: 8 }}
+          />
+        }} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
