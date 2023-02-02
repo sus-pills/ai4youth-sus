9b93dce2e4726ce9245b7194ce6755097347d14e
Mikolaj Pytlak <mikolaj.piotr.pytlak@gmail.com>
2023-02-02 15:00:14 +0100

    Added Nativewind

---
+++ app/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/*.js",
+    "./global/*.js",
+    "./routes/*.js",
+    "./*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "custom-background": "#FFFFFF",
+        "custom-main": "#47B8E0",
+        "custom-secondary": "#134074",
+        "custom-affirmation": "0BC11D",
+        "custom-negation": "F26419",
+      },
+    },
+  },
+  plugins: [],
+};
