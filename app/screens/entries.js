0e7ff586fab99f6c11fc5a7b2e37c4ee7eb4f5ca
Mikolaj Pytlak <mikolaj.piotr.pytlak@gmail.com>
2023-02-02 18:41:30 +0100

    Added button

--- app/screens/entries.js
+++ app/screens/entries.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Textt } from "react-native";
+import IconButton from "../components/iconButton";
 
 // Styles Imports
 import { StyleSheet } from "react-native";
-import { CustomColors } from "../global/globalStyles";
+import { CustomColors, CustomSpacing } from "../global/globalStyles";
 
 const Entries = () => {
+  const [title, setTitle] = useState("Dodaj Wpis");
+
   return (
-    <View>
+    <View style={styles.container}>
+      <IconButton title={title} iconName="add" />
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: CustomColors.customBackground,
+    padding: CustomSpacing.customPadding,
+  },
+});
 
 export default Entries;
