6705b8af0f2c8fb37cbe79709ee6ed718a39193d
Mikolaj Pytlak <mikolaj.piotr.pytlak@gmail.com>
2023-02-03 23:26:06 +0100

    Upgraded entries

--- app/components/entry.js
+++ app/components/entry.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 // Styles Imports
 import { StyleSheet } from "react-native";
@@ -10,18 +11,39 @@ import {
   GlobalStyles,
 } from "../global/globalStyles";
 
+const dateToString = () => {
+
+}
+
 const Entry = ({ entry }) => {
   return (
     <TouchableOpacity
+      activeOpacity={.4}
       style={[
         styles.touchableOpacityButton,
-        { backgroundColor: entry.color },
+        {
+          backgroundColor: "#f6f6f6",
+          borderColor: entry.color,
+        },
         GlobalStyles.customShadow,
       ]}
     >
-      <Text style={styles.testText}>{entry.name}</Text>
-      <Text style={styles.testText}>{entry.datetime}</Text>
-      <Text style={styles.testText}>{entry.period}</Text>
+      <View>
+        <View style={styles.head}>
+          <View style={[styles.colorDot, { backgroundColor: entry.color }]}></View>
+          <Text style={{fontSize: 20}}>{entry.name}</Text>
+        </View>
+
+        <Text style={styles.body}>
+          {"Naciśnij, aby uzyskać więcej informacji"}
+        </Text>
+      </View>
+
+      <MaterialIcons
+        name={"info"}
+        size={40}
+        color="#999"
+      />
     </TouchableOpacity>
   );
 };
@@ -32,10 +54,27 @@ const styles = StyleSheet.create({
     borderRadius: CustomBorder.customRadius,
     padding: 12,
     margin: 6,
+
+    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
-  // testText: {
-  //   color: "white",
-  // },
+  colorDot: {
+    borderWidth: 1,
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    borderColor: 'black',
+    marginRight: 10,
+  },
+  head: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  body: {
+    flex: 1,
+    marginVertical: 6,
+  }
 });
 
 export default Entry;
