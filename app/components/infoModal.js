b3aada2cf887eb1365f8fb2321bc7179941b35c4
Mikolaj Pytlak <mikolaj.piotr.pytlak@gmail.com>
2023-02-12 02:50:49 +0100

    created file

---
+++ app/components/infoModal.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native';
+
+const InfoModal = ({ children }) => {
+
+  return (
+    <View>
+      {children}
+    </View>
+  );
+}
+
+export default InfoModal;
