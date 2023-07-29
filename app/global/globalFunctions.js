faac52493ac8fc1296d7751b9450c783a330c578
Mikolaj Pytlak <mikolaj.piotr.pytlak@gmail.com>
2023-07-29 21:51:41 +0200

    Added predefined storage in globalFunctions

--- app/global/globalFunctions.js
+++ app/global/globalFunctions.js
@@ -1,6 +1,38 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import hexRgb from "hex-rgb";
 
+export const initializeAsyncStorage = async () => {
+  /* 
+    Initializes the async storage component with all keys predefined.
+    * PLEASE! Use the '@' prefix for storage key names, like '@key1' (It is purely conventional).
+    Storage checking / creation follows a simple pattern:
+      Await stored value IF exists || Await new value IF doesn't exist
+  */
+
+  // Default options for the storage
+  const DEFAULT_OPTIONS = {
+    font_size: 16,
+    dark_mode: false,
+  };
+
+  try {
+    // Options
+    await (AsyncStorage.getItem("@options") ||
+      AsyncStorage.setItem("@options", JSON.stringify(DEFAULT_OPTIONS)));
+
+    // Medication Entries
+    await (AsyncStorage.getItem("@entries") ||
+      AsyncStorage.setItem("@entries", JSON.stringify({})));
+
+    // Add other things in the future here
+    // ...
+  } catch (error) {
+    console.log("Error initializing AsyncStorage:", error);
+  }
+};
+
 export const isLightColor = (bgColor) => {
+  /* Checks whether the provided color is light or dark */
 
   const rgb = hexRgb(bgColor ? bgColor : "#000");
 
