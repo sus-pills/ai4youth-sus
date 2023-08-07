2701c13ab384277249511d707f50ee8d0537fef1
Mikolaj Pytlak <mikolaj.piotr.pytlak@gmail.com>
2023-08-07 22:55:29 +0200

    formatted

--- app/screens/entryInfo.js
+++ app/screens/entryInfo.js
@@ -2,8 +2,7 @@ import React from "react";
 import { View, Text } from "react-native";
 import IconButton from "../components/iconButton";
 
-const EntryInfo = ({route, navigation}) => {
-
+const EntryInfo = ({ route, navigation }) => {
   const entry = route.params.entry;
 
   const times = Object.values(entry.times);
@@ -19,14 +18,16 @@ const EntryInfo = ({route, navigation}) => {
       <Text>{`nextIntake: ${entry.nextIntake}`}</Text>
       <Text>{`everyXthDay: ${entry.everyXthDay}`}</Text>
       <Text>{`color: ${entry.color}`}</Text>
-      <IconButton onPress={() => {
-        navigation.navigate("EntryEdit", {entry})
-      }}
-      iconName={'square-edit-outline'}
-      communityIcons={true}
-      title={'Edit'} />
+      <IconButton
+        onPress={() => {
+          navigation.navigate("EntryEdit", { entry });
+        }}
+        iconName={"square-edit-outline"}
+        communityIcons={true}
+        title={"Edit"}
+      />
     </View>
-  ) ; 
-}
+  );
+};
 
 export default EntryInfo;
