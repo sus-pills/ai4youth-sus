2027296b5a7006266b06a8cfdd0b546740fa73b0
Mikolaj Pytlak <mikolaj.piotr.pytlak@gmail.com>
2023-08-20 00:42:13 +0200

    Created the createDateTimes function

--- app/global/globalFunctions.js
+++ app/global/globalFunctions.js
@@ -16,34 +16,35 @@ export const initializeAsyncStorage = async () => {
   };
 
   // ! DELETE THIS LATER
-  const DELETE_THIS_LATER = [{
-    id: "0",
-    name: "Vitamin C",
-    remainingIntakes: 20,
-    startDate: "2023-02-15",
-    dates: {},
-    hours: [
-      "12:00",
-      "18:00"
-    ],
-    dosage: "100 mg",
-    information: "Take after a meal",
-    color: "#f00",
-    icon: "pill",
-  }];
+  const DELETE_THIS_LATER = [
+    {
+      id: "0",
+      name: "Vitamin C",
+      remainingIntakes: 20,
+      startDate: `${new Date()}`,
+      dates: {},
+      hours: ["12:00", "18:00"],
+      dosage: "100 mg",
+      information: "Take after a meal",
+      color: "#f00",
+      icon: "pill",
+    },
+  ];
 
   try {
-
     // ! DELETE THIS LATER
-    await AsyncStorage.clear()
+    await AsyncStorage.clear();
 
     // Options
-    await AsyncStorage.getItem("@options") ||
-      await AsyncStorage.setItem("@options", JSON.stringify(DEFAULT_OPTIONS));
+    (await AsyncStorage.getItem("@options")) ||
+      (await AsyncStorage.setItem("@options", JSON.stringify(DEFAULT_OPTIONS)));
 
     // Medication Entries
-    await AsyncStorage.getItem("@entries") ||
-      await AsyncStorage.setItem("@entries", JSON.stringify(DELETE_THIS_LATER));
+    (await AsyncStorage.getItem("@entries")) ||
+      (await AsyncStorage.setItem(
+        "@entries",
+        JSON.stringify(DELETE_THIS_LATER)
+      ));
 
     // Add other things in the future here
     // ...
@@ -62,6 +63,37 @@ export const isLightColor = (bgColor) => {
   return luminance > 0.5;
 };
 
-export const makeDateTimes = () => {
-  const datetimes = []
-}
+export const createDateTimes = (startDate, hours, remainingIntakes) => {
+  /* generates an array of date-time strings */
+
+  const datetimes = [];
+  // Convert String to Date
+  const formattedDate = new Date(startDate);
+  // Changes an array of "HH:mm" strings to an array of [HH, mm] arrays
+  const formattedHours = hours.map((hour) => hour.split(":"));
+
+  let counter = 0; // keep track of current hour
+  const nextDate = new Date(formattedDate); // copy
+
+  while (datetimes.length < remainingIntakes) {
+
+    // Proceed to the next hour
+    nextDate.setHours(formattedHours[counter][0], formattedHours[counter][1], 0);
+
+    // Append to datetimes if of further date
+    if (nextDate >= formattedDate) {
+      datetimes.push(nextDate.toString());
+    }
+
+    counter += 1;
+
+    // Proceed to the next day if out of hours
+    if (counter === hours.length) {
+      counter = 0;
+      nextDate.setDate(nextDate.getDate() + 1);
+    }
+  }
+
+  return datetimes;
+};
+
